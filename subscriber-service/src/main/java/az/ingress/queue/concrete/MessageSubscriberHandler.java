23c452dc858c355f391c2ff7ae0d3a32f82c4d49
--replace-all <turqudquliyev@gmail.com>
2023-11-11 19:18:55 +0400

    Surrounded with try catch block JsonProcessingException

--- subscriber-service/src/main/java/az/ingress/queue/concrete/MessageSubscriberHandler.java
+++ subscriber-service/src/main/java/az/ingress/queue/concrete/MessageSubscriberHandler.java
@@ -4,9 +4,11 @@ import az.ingress.aop.annotation.Log;
 import az.ingress.model.queue.Subscriber;
 import az.ingress.queue.abstraction.MessageSubscriber;
 import az.ingress.service.abstraction.SubscriberService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -14,6 +16,7 @@ import static az.ingress.mapper.ObjectMapperFactory.OBJECT_MAPPER;
 import static lombok.AccessLevel.PRIVATE;
 
 @Log
+@Slf4j
 @Component
 @RequiredArgsConstructor
 @FieldDefaults(makeFinal = true, level = PRIVATE)
@@ -23,7 +26,11 @@ public class MessageSubscriberHandler implements MessageSubscriber {
     @SneakyThrows
     @RabbitListener(queues = "${rabbitmq.publisher-service.queue}")
     public void subscribe(String message) {
-        var subscriber = OBJECT_MAPPER.getInstance().readValue(message, Subscriber.class);
-        subscriberService.save(subscriber);
+        try {
+            var subscriber = OBJECT_MAPPER.getInstance().readValue(message, Subscriber.class);
+            subscriberService.save(subscriber);
+        } catch (JsonProcessingException e) {
+            log.error("JsonProcessingException: ", e);
+        }
     }
 }
